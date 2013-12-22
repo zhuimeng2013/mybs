cb1857786dca4ac9562f962c6ab53989026a0cab
谢传贵(014823) <chuangui.xiecg@alibaba-inc.com>
2013-12-22 23:43:45 +0800

    login update

--- static/js/pages/login.js
+++ static/js/pages/login.js
@@ -25,4 +25,58 @@ jQuery(function($){
     loginForm.on('submit',function(){
         return validUserName()&&validPwd();
     });
+
+    var num = 1;
+    var isActive = false;
+
+    var cNodes = $("#J-slide-number a");
+
+    function resetTabs(){
+        var node = $("#content  .slide.current");
+        node.removeClass('current');
+
+        node.animate({
+            opacity: 0
+        }, 1000, function() {
+            node.hide();
+            node.find('.bg').removeClass('bg-1');
+        });
+        $("#J-slide-number a").removeClass("slide-number-active"); // Activate first tab
+    }
+
+    function initTab(){
+        var node = $("#content  .slide.current");
+        cNodes.on("click",function(e) {
+            isActive = true;
+            e.preventDefault();
+            if ($(this).hasClass("slide-number-active")){
+                return;
+            }
+            else{
+                resetTabs();
+                $(this).addClass("slide-number-active"); // Activate this
+                var targetTab =  $("#content .slide").eq($(this).data('seed')-1);
+                num = $(this).data('seed');
+                targetTab.addClass('current');
+                targetTab.find('.bg').addClass('bg-1');
+                targetTab.fadeIn().animate({
+                    opacity: 1
+                }, 1000, function() {
+                    isActive = false;
+                });
+            }
+        });
+    }
+    initTab();
+
+    setInterval(function(){
+        if(!isActive){
+            var n = num;
+            if(n==3){
+                n = 0;
+            }
+            cNodes.eq(n).trigger('click');
+        }
+    },4000)
+
 });
